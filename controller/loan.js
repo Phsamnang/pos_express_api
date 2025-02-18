769c8f88eed00820d0a00fd53a147a7f2c23a9da
somnang <somnang.dev@gmail.com>
2025-02-18 14:12:54 +0700

    update

--- controller/loan.js
+++ controller/loan.js
@@ -18,3 +18,25 @@ exports.createLoan = async (req, res) => {
         res.status(500).json({error:"Failed to create loan"})
     }
 }
+
+exports.getLoanByUserIdAndDateBetween = async (req,res)=>{
+
+    try {
+        const {userId,startDate,endDate} = req.params;
+
+       console.log(req.params);
+       
+        const loans = await Laon.findAll({
+            where:{
+                userId,
+                loadDate:{
+                    [between]:[startDate,endDate]
+                }
+            }
+        })
+        res.status(200).json({loans})
+    } catch (error) {
+
+        res.status(500).json({error:"Failed to get loans by date"})
+    }
+}   
--- router/laon.js
+++ router/laon.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {createLoan} = require("../controller/loan");
+const {createLoan, getLoanByUserIdAndDateBetween} = require("../controller/loan");
 
 router.post("/create-loan", createLoan);
+router.get("/get-loan/:userId/:startDate?/:endDate?", getLoanByUserIdAndDateBetween);
 
 module.exports=router
